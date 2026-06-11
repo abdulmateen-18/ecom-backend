39256917d5f12b03be83851b801a483267578f3a
abduljabbarkhanh0-cpu <mohammedabdulmateenkhan18@gmail.com>
2026-06-11 15:51:53 +0530

    main.py change

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ app.add_middleware(SlowAPIMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # for now (later restrict)
+    allow_origins=["*"],  # for now (later restrict)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
